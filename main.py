70f981aae176ba793aa2b6e822377f5684f7832c
Bhuvi <rambhuvaneshreddy@gmail.com>
2025-12-29 10:57:34 +0530

    test

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ if choice == "1":
         print("Task added successfully.")
     else:
         print("empty task not allowed.")
+
+if choice == "1":
+    task = input("enter task:").strip()
+    if task:
+        tasks.append(task)
+        print("Task added successfully.")
+    else:
+        print("empty task not allowed.")
